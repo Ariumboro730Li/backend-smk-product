bdebba6c214508f12e1dfa8442f6de5051fbbd81
Ari Umboro Seno <ariumboroseno@gmail.com>
2025-01-10 18:05:22 +0700

    role permission

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use DB;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class RoleMiddleware
@@ -20,6 +21,21 @@ class RoleMiddleware
                 'error' => true,
                 'message' => 'Maaf, Anda tidak diizinkan mengakses.'
             ], 403);
+        }
+
+        if($role == 'internal'){
+            $roleModel = DB::table('model_has_roles')->where('model_id', $user->id)
+            ->join('roles', 'model_has_roles.role_id', '=', 'roles.id')
+            ->first();
+
+            if($roleModel){
+                $roleId = $roleModel->role_id;
+                $permissionRole = DB::table('role_has_permissions')
+                ->select('name', 'group', 'guard_name')
+                ->join('permissions', 'role_has_permissions.permission_id', '=', 'permissions.id')
+                ->where('role_id', $roleId)->get()->toArray()   ;
+                $request->permission = $permissionRole;
+            }
         }
 
         return $next($request);
